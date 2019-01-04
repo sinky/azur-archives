52cef1dc51773b77c320d644a7367c94b29d036c
Marco Krage <marco@my-azur.de>
2019-01-04 13:11:55 +0100

    Update azur-archives.php

--- azur-archives.php
+++ azur-archives.php
@@ -4,7 +4,7 @@ Plugin Name: Azur Archives
 Plugin URI: https://github.com/sinky/azur-archives
 Version: 1.0
 Author: Marco Krage
-Author URI: http://my-azur.de
+Author URI: https://my-azur.de
 Description: Display fancy archive list by shortcode
 */
 
